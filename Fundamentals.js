39b6639d413112fe7ff6a68076981ac95b0fc570
Jhoel Olivera <jhoelolivera165@gmail.com>
2025-07-15 20:53:30 -0400

    second commit

---
+++ Fundamentals.js
@@ -0,0 +1,30 @@
+//comments
+/*We can use this for comment a lot of words*/
+
+//Variables
+
+    var num = 5
+    num = cinco
+
+//  var+name= type 
+
+//we have 3 types to declare a variables
+// var let const
+
+    let number = 1542
+    const pi =3.14151698
+////////////////
+
+var a = 5;
+var b = 10;
+var c = jhoel;
+
+/////////////
+
+a = a + 5;
+b = b - 5;
+c = c + olivera ;
+
+console.log(a);
+console.log(b);
+console.log(c);
