9325823f6bea479bcb85299840659d21851b4613
Jhoel Olivera <jhoelolivera165@gmail.com>
2025-07-16 21:42:08 -0400

    second 3 commit

---
+++ practiceFundamentalsString.js
@@ -0,0 +1,60 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+
+let bot;
+let botLocation;
+
+bot = "teacherBot";
+botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!")
+
+////////////////////////////
+
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "funBot";
+let botLocation = "yourHearth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = favoriteLanguage+" is use for websites.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage+" is the best.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage+" Everyone use that beacuse"+".";
+console.log(codingFact);
+
+codingFact = favoriteLanguage+" I love you"+".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+ botLocation+".");
+
