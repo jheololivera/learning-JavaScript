eac93f93c93e52e78779695688993ab438a22f42
Jhoel Olivera <jhoelolivera165@gmail.com>
2025-07-18 21:50:34 -0400

    second 5 commit

--- Strings.js
+++ Strings.js
@@ -2,3 +2,11 @@ let string = "Jhoel olivera";
 console.log(string);
 console.log(string.length);
 console.log(string[0]);
+
+let name = prompt("What is your name","que es esto");
+console.log(name);
+
+let a = "0123456789";
+console.log(a[0]);
+console.log(a[a.length-1]);
+console.log(a[-1]);
